40d6683b1ac12218333b2a73a28cb84fe072660f
Misho <nikolovmisho5@gmail.com>
2023-12-12 22:48:50 +0200

    Token commit

---
+++ script/DeployOurToken.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {OurToken} from "../src/OurToken.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeployOurToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000 ether;
+
+    function run() external returns (OurToken) {
+        vm.startBroadcast();
+        OurToken ot = new OurToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+        return ot;
+    }
+}
